750ace1d2407b963331fe627cbdc83ff13a5dcb7
Diego Inácio <62408076+iamdiegoinacio@users.noreply.github.com>
2021-03-27 18:25:52 -0300

    Update nkninstall.sh

--- nkninstall.sh
+++ nkninstall.sh
@@ -1,44 +1,26 @@
 #!/bin/bash
-func_update () {
-    sudo apt-get update -y
-}
-func_upgrade () {
-    sudo apt-get upgrade -y
-}
-func_deletechaindb () {
-    cd ..
-    cd nkn/nkn-commercial/services/nkn-node/
-    rm -r ChainDB
-}
-func_downloadlatestchaindb (){
-    wget https://nkn.org/ChainDB_pruned_latest.tar.gz
-}
-func_unziplatestchaindb (){
-    tar -vzxf ChainDB_pruned_latest.tar.gz
-}
-func_deletedownloadchaindb () {
-    rm -r ChainDB_pruned_latest.tar.gz
-}
 echo -e "\033[1;36mLINUX UPGRADE RUNNING\033[0m"
 echo ""
 echo -e "\033[1;33mWAIT..."
-func_update()
+sudo apt-get update -y
 echo -e "\033[1;36mLINUX UPGRADE RUNNING\033[0m"
 echo ""
 echo -e "\033[1;33mWAIT..."
-func_upgrade()
+sudo apt-get upgrade -y
 echo -e "\033[1;36mDELETING THE OLD DATABASE\033[0m"
 echo ""
-func_deletechaindb()
+cd ..
+cd nkn/nkn-commercial/services/nkn-node/
+rm -r ChainDB
 echo -e "\033[1;36mDOWNLOAD THE NEW DATABASE\033[0m"
 echo ""
 echo -e "\033[1;33mWAIT..."
-func_downloadlatestchaindb()
+wget https://nkn.org/ChainDB_pruned_latest.tar.gz
 echo -e "\033[1;36mUNZIP DATABASE\033[0m"
 echo ""
 echo -e "\033[1;33mWAIT..."
-func_unziplatestchaindb()
+tar -vzxf ChainDB_pruned_latest.tar.gz
 echo -e "\033[1;36mREMOVING TEMP DATABASE FILES\033[0m"
 echo ""
 echo -e "\033[1;33mWAIT..."
-func_deletedownloadchaindb()
+rm -r ChainDB_pruned_latest.tar.gz
